9c63f30ec1b5e8fa0b9bf1a665b4d3fcfdbe0a5f
yurem <Yuriy.Movchan@gmail.com>
2015-06-23 21:34:49 +0300

    Fix patches in setup script

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ class Setup(object):
     def __init__(self, install_dir=None):
         self.install_dir = install_dir
 
-        self.oxVersion = '3.0.0-SNAPSHOT'
-        self.githubBranchName = 'master'
+        self.oxVersion = '2.3.0.Final'
+        self.githubBranchName = 'version_2.3'
 
         # Used only if -w (get wars) options is given to setup.py
         self.oxtrust_war = 'https://ox.gluu.org/maven/org/xdi/oxtrust-server/%s/oxtrust-server-%s.war' % (self.oxVersion, self.oxVersion)
