98c77450485315076bf0593c2148301a96401ad3
yurem <Yuriy.Movchan@gmail.com>
2016-02-12 21:41:20 +0200

    Version 2.4.2.Final

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ class Setup(object):
     def __init__(self, install_dir=None):
         self.install_dir = install_dir
 
-        self.oxVersion = '3.0.3-SNAPSHOT'
-        self.githubBranchName = 'master'
+        self.oxVersion = '2.4.2.Final'
+        self.githubBranchName = 'version_2.4.2'
 
         # Used only if -w (get wars) options is given to setup.py
         self.oxtrust_war = 'https://ox.gluu.org/maven/org/xdi/oxtrust-server/%s/oxtrust-server-%s.war' % (self.oxVersion, self.oxVersion)
