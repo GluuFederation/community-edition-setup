7ace852a9eaa29d0d5735a38b6abe75f2f76150b
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2017-12-28 18:23:53 +0300

    Final 3.1.2 packages

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class Setup(object):
     def __init__(self, install_dir=None):
         self.install_dir = install_dir
 
-        self.oxVersion = '3.1.2-SNAPSHOT'
+        self.oxVersion = '3.1.2.Final'
         self.githubBranchName = 'version_3.1.2'
 
         # Used only if -w (get wars) options is given to setup.py
