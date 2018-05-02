346d273f23e39d6c858deb1e04839ed6c001f68f
yurem <Yuriy.Movchan@gmail.com>
2018-05-02 12:13:44 +0300

    Version 3.1.4

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class Setup(object):
     def __init__(self, install_dir=None):
         self.install_dir = install_dir
 
-        self.oxVersion = '3.1.3.Final'
+        self.oxVersion = '3.1.4-SNAPSHOT'
         self.githubBranchName = 'version_3.1.3'
 
         # Used only if -w (get wars) options is given to setup.py
