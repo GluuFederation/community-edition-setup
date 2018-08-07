217c78ef808d3a5a24d5c2d854601811102a1252
David <nikdavnik@gmail.com>
2018-08-08 00:27:06 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -811,16 +811,11 @@ class Setup(object):
         return inFilePathText
 
     def insertLinesInFile(self, inFilePath, index, text):        
-            inFilePathLines = None        
-            
+            inFilePathLines = None                    
             try:            
                 f = open(inFilePath, "r")            
                 inFilePathLines = f.readlines()            
-                f.close()        
-            except:            
-                self.logIt("Error reading %s" % inFilePathLines, True)
-                self.logIt(traceback.format_exc(), True)        
-
+                f.close()
                 try:
                     self.backupFile(inFilePath)
                     inFilePathLines.insert(index, text)            
@@ -831,6 +826,9 @@ class Setup(object):
                 except:            
                     self.logIt("Error writing %s" % inFilePathLines, True)            
                     self.logIt(traceback.format_exc(), True)
+            except:            
+                self.logIt("Error reading %s" % inFilePathLines, True)
+                self.logIt(traceback.format_exc(), True)        
                     
     def commentOutText(self, text):
         textLines = text.split('\n')
@@ -2905,7 +2903,7 @@ class Setup(object):
         else:
             self.run([self.ldapDsCreateRcCommand, "--outputFile", "/etc/init.d/opendj", "--userName",  "ldap"])
             # Make the generated script LSB compliant            
-            lsb_str="""#### BEGIN INIT INFO
+            lsb_str="""### BEGIN INIT INFO
 # Provides:          opendj
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
@@ -2913,8 +2911,8 @@ class Setup(object):
 # Default-Stop:      0 1 6
 # Short-Description: Start daemon at boot time
 # Description:       Enable service provided by daemon.
-### END INIT INFO"""            
-            
+### END INIT INFO
+"""                        
             self.insertLinesInFile("/etc/init.d/opendj", 1, lsb_str)
         
         if self.os_type in ['centos', 'fedora', 'red']:
