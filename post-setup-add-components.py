0ba1c15bf927e0beccf1e21db4b10ee5f387021f
Mustafa Baser <mbaser@mail.com>
2020-02-26 22:31:20 +0300

    fix post setup installations

--- post-setup-add-components.py
+++ post-setup-add-components.py
@@ -7,14 +7,7 @@ import json
 import zipfile
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-
-if not os.path.exists('setup.py'):
-    print "This script should be run from /install/community-edition-setup/"
-    sys.exit()
-    
-if not os.path.exists('/install/community-edition-setup/setup.properties.last'):
-    print "setup.properties.last is missing can't continue"
-    sys.exit()
+ces_dir = os.path.join(cur_dir, 'ces_current')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-addshib", help="Install Shibboleth SAML IDP", action="store_true")
@@ -29,26 +22,13 @@ if  len(sys.argv)<2:
     parser.exit(1)
 
 def get_properties(prop_fn):
-    p = Properties.Properties()
-    with open(prop_fn) as file_object:
-        p.load(file_object)
-    return p
+    jprop = JProperties()
+    with open(prop_fn) as f:
+        jprop.load(f)
 
-oxVersion = 0
-setup_properties_fn = '/install/community-edition-setup/setup.properties.last'
-install_dir = '.'
-
-#Determine setup version
-with open(setup_properties_fn) as f:
-    for l in f:
-        ls = l.strip()
-        if ls.startswith('oxVersion'):
-            n = ls.find('=')
-            oxVersion_setup = ls[n+1:].strip()
+    return jprop
 
 run_oxauth_war_fn = '/opt/gluu/jetty/oxauth/webapps/oxauth.war'
-
-os.system('cp -f {} /opt/dist/gluu'.format(run_oxauth_war_fn))
 
 #Determine gluu version
 war_zip = zipfile.ZipFile(run_oxauth_war_fn, 'r')
@@ -68,81 +48,51 @@ for l in menifest.splitlines():
 
 print "Current Gluu Version", gluu_version
 
-ces_version_l = []
-for ci in oxVersion_current.split('.'):
-    if ci.lower() == 'final' or ci.lower().startswith('sp') or ci.lower().startswith('patch'):
-        continue
-    ces_version_l.append(ci)
-    
-ces_version = '.'.join(ces_version_l)
 
+if os.path.exists(ces_dir + '.back'):
+    os.system('rm -r -f ' + ces_dir + '.back')
 
-if os.path.exists('ces_current.back'):
-    os.system('rm -r -f ces_current.back')
+if os.path.exists(ces_dir):
+    os.system('mv {0} {0}.back'.format(ces_dir))
 
-if os.path.exists('ces_current'):
-    os.system('mv ces_current ces_current.back')
+ces_url = 'https://github.com/GluuFederation/community-edition-setup/archive/version_{}.zip'.format(gluu_version)
+print "Downloading Community Edition Setup {}".format(gluu_version)
 
-ces_url = 'https://github.com/GluuFederation/community-edition-setup/archive/version_{}.zip'.format(ces_version)
-
-print "Downloading Community Edition Setup {}".format(ces_version)
-
-os.system('wget -nv {} -O version_{}.zip'.format(ces_url, ces_version))
+os.system('wget -nv {} -O {}/version_{}.zip'.format(ces_url, cur_dir, gluu_version))
 print "Extracting package"
-os.system('unzip -o -qq version_{}.zip'.format(ces_version))
-os.system('mv community-edition-setup-version_{} ces_current'.format(ces_version))
+os.system('unzip -o -qq {}/version_{}.zip'.format(cur_dir, gluu_version))
+os.system('mv {}/community-edition-setup-version_{} {}/ces_current'.format(cur_dir, gluu_version, cur_dir))
+os.system('wget -nv https://raw.githubusercontent.com/GluuFederation/community-edition-setup/master/pylib/generate_properties.py -O {}'.format(os.path.join(ces_dir, 'pylib', 'generate_properties.py')))
 
+open(os.path.join(cur_dir, 'ces_current/__init__.py'),'w').close()
 
-open('ces_current/__init__.py','w').close()
-
-sys.path.append('ces_current')
+sys.path.append(ces_dir)
 
 from ces_current.setup import *
-install_dir = 'ces_current'
+from ces_current.pylib.cbm import CBM
+from ces_current.pylib.generate_properties import generate_properties
+from ces_current.pylib.Properties import Properties as JProperties
 
-setupObj = Setup(install_dir)
+setup_porperties = generate_properties(True)
+
+setupObj = Setup(ces_dir)
 
 setupObj.setup = setupObj
 
 setupObj.os_type, setupObj.os_version = setupObj.detect_os_type()
 setupObj.os_initdaemon = setupObj.detect_initd()
 
-
-if oxVersion != gluu_version:
-
-    keep_keys = ['idp3_war', 'idp3_cml_keygenerator', 'idp3_dist_jar', 'idp3Folder',
-                'idp3MetadataFolder', 'idp3MetadataCredentialsFolder', 'idp3LogsFolder',
-                'idp3LibFolder', 'idp3ConfFolder', 'idp3ConfAuthnFolder', 
-                'idp3CredentialsFolder', 'idp3WebappFolder', 'oxVersion',
-                'templateFolder', 'outputFolder',
-                'ldif_passport_config', 'ldif_passport', 'ldif_passport_clients',
-                ]
-    keep_dict = {}
-
-    for k in keep_keys:
-        v = getattr(setupObj, k)
-        if v:
-            keep_dict[k] = v
-
-setupObj.load_properties('/install/community-edition-setup/setup.properties.last')
-
-if oxVersion != gluu_version:
-    for k in keep_dict:
-        setattr(setupObj, k, keep_dict[k])
-
-
-setupObj.githubBranchName = oxVersion
+for setup_key in setup_porperties:
+    setattr(setupObj, setup_key, setup_porperties[setup_key])
 
 setupObj.log = os.path.join(setupObj.install_dir, 'post_setup.log')
 setupObj.logError = os.path.join(setupObj.install_dir, 'post_setup_error.log')
 
 print "Log Files:", setupObj.log, setupObj.logError
 
-attribDataTypes.startup(install_dir)
+attribDataTypes.startup(ces_dir)
 
-if oxVersion == gluu_version:
-    setupObj.cbm = CBM(setupObj.couchbase_hostname, setupObj.couchebaseClusterAdmin, setupObj.ldapPass)
-
+gluu_cb_prop_fn = '/etc/gluu/conf/gluu-couchbase.properties'
 
 if not hasattr(setupObj, 'ldap_type'):
     setupObj.ldap_type = 'open_ldap'
@@ -254,6 +204,42 @@ def installSaml():
     setupObj.run([setupObj.cmd_chown, '-R', 'jetty:jetty', '/var/run/jetty'])
     setupObj.enable_service_at_start('idp')
 
+    if persistence_type == 'ldap':
+        ox_ldap_prop = get_properties('/etc/gluu/conf/gluu-ldap.properties')
+
+        bindDN = ox_ldap_prop['bindDN']
+        bindPassword_e = ox_ldap_prop['bindPassword']
+        cmd = '/opt/gluu/bin/encode.py -D ' + bindPassword_e    
+        bindPassword = os.popen(cmd).read().strip()
+        ldap_host_port = ox_ldap_prop['servers'].split(',')[0].strip()
+
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
+        ldap_conn = ldap.initialize('ldaps://'+ldap_host_port)
+        ldap_conn.simple_bind_s(bindDN, bindPassword)
+
+        result = ldap_conn.search_s(
+                        'o=gluu',
+                        ldap.SCOPE_SUBTREE,
+                        '(objectClass=oxTrustConfiguration)',
+                        ['oxTrustConfApplication']
+                        )
+
+        dn = result[0][0]
+        oxTrustConfApplication = json.loads(result[0][1]['oxTrustConfApplication'][0])
+        
+        oxTrustConfApplication['configGeneration'] = True
+        oxTrustConfApplication_js = json.dumps(oxTrustConfApplication, indent=2)
+
+        ldap_conn.modify_s(
+                        dn,
+                        [( ldap.MOD_REPLACE, 'oxTrustConfApplication',  oxTrustConfApplication_js)]
+                    )
+
+    else:
+        bucket = gluu_cb_prop['bucket.default']
+        setupObj.cbm.exec_query('UPDATE `{}` USE KEYS "configuration_oxtrust" SET configGeneration=true'.format(bucket))
+        
+
     print "Shibboleth installation done"
 
 
@@ -264,37 +250,35 @@ def installPassport():
         print "Passport is already installed on this system"
         return
 
-    if oxVersion != gluu_version:
+    node_url = 'https://nodejs.org/dist/v{0}/node-v{0}-linux-x64.tar.xz'.format(setupObj.node_version)
+    nod_archive_fn = os.path.basename(node_url)
 
-        node_url = 'https://nodejs.org/dist/v{0}/node-v{0}-linux-x64.tar.xz'.format(setupObj.node_version)
-        nod_archive_fn = os.path.basename(node_url)
+    print "Downloading {}".format(nod_archive_fn)
+    setupObj.run(['wget', '-nv', node_url, '-O', os.path.join(setupObj.distAppFolder, nod_archive_fn)])
+    cur_node_dir = os.readlink('/opt/node')
+    setupObj.run(['unlink', '/opt/node'])
+    setupObj.run(['mv', cur_node_dir, cur_node_dir+'.back'])
 
-        print "Downloading {}".format(nod_archive_fn)
-        setupObj.run(['wget', '-nv', node_url, '-O', os.path.join(setupObj.distAppFolder, nod_archive_fn)])
-        cur_node_dir = os.readlink('/opt/node')
-        setupObj.run(['unlink', '/opt/node'])
-        setupObj.run(['mv', cur_node_dir, cur_node_dir+'.back'])
+    print "Installing", nod_archive_fn
+    setupObj.installNode()
 
-        print "Installing", nod_archive_fn
-        setupObj.installNode()
+    passport_url = 'https://ox.gluu.org/npm/passport/passport-{}.tgz'.format(gluu_version)
+    passport_modules_url = 'https://ox.gluu.org/npm/passport/passport-version_{}-node_modules.tar.gz'.format(gluu_version)
+    passport_fn = os.path.basename(passport_url)
+    passport_modules_fn = os.path.basename(passport_modules_url)
 
-        passport_url = 'https://ox.gluu.org/npm/passport/passport-{}.tgz'.format(gluu_version)
-        passport_modules_url = 'https://ox.gluu.org/npm/passport/passport-version_{}-node_modules.tar.gz'.format(gluu_version)
-        passport_fn = os.path.basename(passport_url)
-        passport_modules_fn = os.path.basename(passport_modules_url)
+    print "Downloading {}".format(passport_fn)
+    setupObj.run(['wget', '-nv', passport_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport.tgz')])
 
-        print "Downloading {}".format(passport_fn)
-        setupObj.run(['wget', '-nv', passport_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport.tgz')])
+    print "Downloading {}".format(passport_modules_fn)
+    setupObj.run(['wget', '-nv', passport_modules_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport-node_modules.tar.gz')])
 
-        print "Downloading {}".format(passport_modules_fn)
-        setupObj.run(['wget', '-nv', passport_modules_url, '-O', os.path.join(setupObj.distGluuFolder, 'passport-node_modules.tar.gz')])
-
-        if setupObj.os_initdaemon == 'systemd':
-            passport_syatemd_url = 'https://raw.githubusercontent.com/GluuFederation/community-edition-package/master/package/systemd/passport.service'
-            passport_syatemd_fn = os.path.basename(passport_syatemd_url)
-            
-            print "Downloading {}".format(passport_syatemd_fn)
-            setupObj.run(['wget', '-nv', passport_syatemd_url, '-O', '/usr/lib/systemd/system/passport.service'])
+    if setupObj.os_initdaemon == 'systemd':
+        passport_syatemd_url = 'https://raw.githubusercontent.com/GluuFederation/community-edition-package/master/package/systemd/passport.service'
+        passport_syatemd_fn = os.path.basename(passport_syatemd_url)
+        
+        print "Downloading {}".format(passport_syatemd_fn)
+        setupObj.run(['wget', '-nv', passport_syatemd_url, '-O', '/usr/lib/systemd/system/passport.service'])
 
 
 
@@ -328,32 +312,29 @@ def installOxd():
         return
     
     print "Installing Oxd Server"
-    
-    if oxVersion != gluu_version:
 
-        oxd_url = 'https://ox.gluu.org/maven/org/gluu/oxd-server/{0}/oxd-server-{0}-distribution.zip'.format(oxVersion_current)
+    oxd_url = 'https://ox.gluu.org/maven/org/gluu/oxd-server/{0}/oxd-server-{0}-distribution.zip'.format(oxVersion_current)
 
-        print "Downloading {} and preparing package".format(os.path.basename(oxd_url))
-        oxd_zip_fn = '/tmp/oxd-server.zip'
-        oxd_tmp_dir = '/tmp/oxd-server'
+    print "Downloading {} and preparing package".format(os.path.basename(oxd_url))
+    oxd_zip_fn = '/tmp/oxd-server.zip'
+    oxd_tmp_dir = '/tmp/oxd-server'
 
-        setupObj.run(['wget', '-nv', oxd_url, '-O', oxd_zip_fn])
-        setupObj.run(['unzip', '-qqo', '/tmp/oxd-server.zip', '-d', oxd_tmp_dir])
-        setupObj.run(['mkdir', os.path.join(oxd_tmp_dir,'data')])
+    setupObj.run(['wget', '-nv', oxd_url, '-O', oxd_zip_fn])
+    setupObj.run(['unzip', '-qqo', '/tmp/oxd-server.zip', '-d', oxd_tmp_dir])
+    setupObj.run(['mkdir', os.path.join(oxd_tmp_dir,'data')])
 
-        if setupObj.os_type + setupObj.os_version in ('ubuntu18','debian9'):
-            default_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/oxd-server-default'.format(ces_version)
-            setupObj.run(['wget', '-nv', default_url, '-O', os.path.join(oxd_tmp_dir, 'oxd-server-default')])
+    if setupObj.os_type + setupObj.os_version in ('ubuntu18','debian9'):
+        default_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/oxd-server-default'.format(gluu_version)
+        setupObj.run(['wget', '-nv', default_url, '-O', os.path.join(oxd_tmp_dir, 'oxd-server-default')])
 
-        service_file = 'oxd-server.init.d' if setupObj.os_type + setupObj.os_version in ('ubuntu18','debian9') else 'oxd-server.service'
-        service_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/{}.file'.format(ces_version, service_file)
-        setupObj.run(['wget', '-nv', service_url, '-O', os.path.join(oxd_tmp_dir, service_file)])
+    service_file = 'oxd-server.init.d' if setupObj.os_type + setupObj.os_version in ('ubuntu18','debian9') else 'oxd-server.service'
+    service_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/{}.file'.format(gluu_version, service_file)
+    setupObj.run(['wget', '-nv', service_url, '-O', os.path.join(oxd_tmp_dir, service_file)])
 
-        oxd_server_sh_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/oxd-server.sh'.format(ces_version)
-        setupObj.run(['wget', '-nv', oxd_server_sh_url, '-O', os.path.join(oxd_tmp_dir, 'bin/oxd-server.sh')])
+    oxd_server_sh_url = 'https://raw.githubusercontent.com/GluuFederation/oxd/version_{}/debian/oxd-server.sh'.format(gluu_version)
+    setupObj.run(['wget', '-nv', oxd_server_sh_url, '-O', os.path.join(oxd_tmp_dir, 'bin/oxd-server.sh')])
 
-        setupObj.run(['tar', '-zcf', os.path.join(setupObj.distGluuFolder, 'oxd-server.tgz'), 'oxd-server'], cwd='/tmp')
-    
+    setupObj.run(['tar', '-zcf', os.path.join(setupObj.distGluuFolder, 'oxd-server.tgz'), 'oxd-server'], cwd='/tmp')
 
     setupObj.oxd_package = os.path.join(setupObj.distGluuFolder, 'oxd-server.tgz')
     setupObj.install_oxd()
@@ -374,12 +355,12 @@ def installCasa():
     setupObj.prepare_base64_extension_scripts()
 
     casa_script_fn = os.path.basename(setupObj.ldif_scripts_casa)
-    casa_script_fp = os.path.join(cur_dir, 'ces_current/output', casa_script_fn)
+    casa_script_fp = os.path.join(ces_dir, 'output', casa_script_fn)
     
     setupObj.renderTemplateInOut(
-                    os.path.join(cur_dir, 'ces_current/templates/', casa_script_fn),
-                    os.path.join(cur_dir, 'ces_current/templates'),
-                    os.path.join(cur_dir, 'ces_current/output'),
+                    os.path.join(ces_dir, 'templates', casa_script_fn),
+                    os.path.join(ces_dir, 'templates'),
+                    os.path.join(ces_dir, 'output'),
                     )
 
     if persistence_type == 'ldap':
@@ -396,9 +377,9 @@ def installCasa():
     setupObj.import_oxd_certificate()
 
     setupObj.renderTemplateInOut(
-                    os.path.join(cur_dir, 'ces_current/templates/casa.json'),
-                    os.path.join(cur_dir, 'ces_current/templates'),
-                    os.path.join(cur_dir, 'ces_current/output'),
+                    os.path.join(ces_dir, 'templates/casa.json'),
+                    os.path.join(ces_dir, 'templates'),
+                    os.path.join(ces_dir, 'output'),
                     )
     setupObj.calculate_selected_aplications_memory()
     setupObj.install_casa()
