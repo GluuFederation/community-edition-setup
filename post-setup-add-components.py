7bb20bd3236d536f93accf2c6548d08e6ccd145c
Mustafa Baser <mbaser@mail.com>
2021-04-06 14:41:38 +0300

    fix: post setup fixes

--- post-setup-add-components.py
+++ post-setup-add-components.py
@@ -97,12 +97,13 @@ class ProgressBar:
 
 setup_porperties = generate_properties(True)
 
+os.rename('/opt/dist/gluu/oxauth-client-jar-with-dependencies.jar', '/opt/dist/gluu/oxauth-client-jar-with-dependencies.jar.old')
 
 setup.__dict__['ruamel'] = ruamel
 setupObj = setup.Setup(ces_dir)
+setupObj.oxVersion = gluu_version +'.Final'
 setupObj.initialize()
 setupObj.pbar = ProgressBar()
-setupObj.oxVersion = gluu_version 
 
 setupObj.setup = setupObj
 
