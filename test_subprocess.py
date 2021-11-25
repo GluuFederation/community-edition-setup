35679b5813ba19afa9a7d067306053118c97dafe
Mustafa Baser <mbaser@mail.com>
2021-11-25 15:04:24 +0300

    fix: use shell=True

--- test_subprocess.py
+++ test_subprocess.py
@@ -3,11 +3,11 @@ import shlex
 
 
 def run(args):
-    if isinstance(args, str):
-        args = shlex.split(args)
-    p = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    if not isinstance(args, str):
+        args = ' '.join(args)
+    p = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return p.stdout.decode()
 
-result = run('/usr/bin/ls -al')
+result = run(('/usr/bin/ls', '-al'))
 
 print(result)
