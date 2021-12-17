a1758e129facad5eb5e0d287e27a62ba2256e00a
nikdavnik <nikdavnik@gmail.com>
2021-12-17 17:44:33 +0400

    changes war to .Final

--- gluu_install.py
+++ gluu_install.py
@@ -158,7 +158,7 @@ services = ['casa.service', 'identity.service', 'opendj.service', 'oxauth.servic
 app_versions = {
     "JETTY_VERSION": "9.4.44.v20210927", 
     "AMAZON_CORRETTO_VERSION": "11.0.13.8.1", 
-    "OX_GITVERISON": "-SNAPSHOT", 
+    "OX_GITVERISON": ".Final", 
     "NODE_VERSION": "v14.16.1",
     "OX_VERSION": "4.3.1", 
     "PASSPORT_VERSION": "4.3.1", 
