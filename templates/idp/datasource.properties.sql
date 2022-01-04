d5a130b15e20f8ef080f788b90db42cc426ea1aa
yurem <Yuriy.Movchan@gmail.com>
2022-01-04 22:06:39 +0300

    fix: upate idp conf files

--- templates/idp/datasource.properties.sql
+++ templates/idp/datasource.properties.sql
@@ -1,5 +1,5 @@
 idp.attribute.resolver.datasource.driverClass=com.%(rdbm_type)s.jdbc.Driver
-idp.attribute.resolver.datasource.jdbcUrl=%(rdbm_type)s://%(rdbm_host)s:%(rdbm_port)s/%(rdbm_db)s?enabledTLSProtocols=TLSv1.2
+idp.attribute.resolver.datasource.jdbcUrl=jdbc:%(rdbm_type)s://%(rdbm_host)s:%(rdbm_port)s/%(rdbm_db)s
 idp.attribute.resolver.datasource.user=%(rdbm_user)s
 idp.attribute.resolver.datasource.password=%(rdbm_password)s
 idp.attribute.resolver.datasource.serverTimezone=%(server_time_zone)s
