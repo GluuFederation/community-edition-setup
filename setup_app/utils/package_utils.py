b772c106e5a5654b52e58c6d6809546f56aed07a
Mustafa Baser <mbaser@mail.com>
2022-08-06 16:27:27 +0300

    fix: suse package list

--- setup_app/utils/package_utils.py
+++ setup_app/utils/package_utils.py
@@ -19,8 +19,12 @@ class PackageUtils(SetupUtils):
             check_text = '0'
 
         elif base.clone_type == 'rpm':
-            install_command = 'yum install -y {0}'
-            update_command = 'yum install -y epel-release'
+            if base.os_type == 'suse':
+                install_command = 'zypper install -y {0}'
+                update_command = 'zypper refresh'
+            else:
+                install_command = 'yum install -y {0}'
+                update_command = 'yum install -y epel-release'
             query_command = 'rpm -q {0}'
             check_text = 'is not installed'
 
